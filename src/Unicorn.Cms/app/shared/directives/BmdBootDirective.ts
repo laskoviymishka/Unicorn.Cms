8d1813c029a8cb53894b4b836f631871d6ffda6e
Andrei Tserakhau <andrei_tserakhau@epam.com>
2016-05-18 10:46:32 +0300

    feat: add bmd boot directive

---
+++ src/Unicorn.Cms/app/shared/directives/BmdBootDirective.ts
@@ -0,0 +1,15 @@
+﻿import { Directive, ElementRef, OnInit } from '@angular/core';
+
+declare var jQuery: any;
+
+@Directive({
+  selector: '[bmdBoot]'
+})
+export class BmdBootDirective implements OnInit {
+
+  constructor(private _element: ElementRef) { }
+
+  ngOnInit(): void {
+    jQuery(this._element).bootstrapMaterialDesign();
+  }
+}
--- src/Unicorn.Cms/app/shared/directives/controls/dropdownField.ts
+++ src/Unicorn.Cms/app/shared/directives/controls/dropdownField.ts
@@ -1,11 +1,14 @@
 import * as ng from '@angular/core';
 import { getMeta, IMetadata } from '../../domain';
+import { BmdBootDirective } from '../BmdBootDirective';
+import { BaseField } from './baseField';
 
 @ng.Component({
   selector: 'dropdown-field',
   template: require('./dropdownField.html'),
+  directives: [BmdBootDirective]
 })
-export class DropdownField implements ng.OnInit {
+export class DropdownField extends BaseField implements ng.OnInit {
   ngOnInit(): void {
   }
 }
